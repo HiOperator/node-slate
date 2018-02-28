e51641a9b63b50233e4b606a1d5d4098efb44825
John Roman <jrrmod@gmail.com>
2018-02-28 12:24:26 -0500

    adding yarn-error.log to the .gitignore, and updating the aws sync command

--- deploy.sh
+++ deploy.sh
@@ -5,11 +5,15 @@ set -o nounset
 
 readonly AWS_PROFILE=$1
 readonly AWS_BUCKET="s3://documentation.hioperator.com"
+readonly BUILD_DIR="./build"
+
+if [ -d "${BUILD_DIR}" ]; then
+    rm -r "${BUILD_DIR}"
+fi
 # check if user has yarn
 # if not use npm to build the documentation
-if [ -z `which yarn` ]; do
+if [ -z `which yarn` ]; then
     npm run build
 fi
 yarn run build
-cd ./build
-aws --profile "${AWS_PROFILE}" s3 sync "${AWS_BUCKET}"
+aws --profile "${AWS_PROFILE}" s3 sync "${BUILD_DIR}/." "${AWS_BUCKET}"
