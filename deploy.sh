e79c0eb6c9c70ddc16c9487bbab799a55cc2cccf
John Roman <jrrmod@gmail.com>
2018-02-28 14:32:35 -0500

    adding some error checking

--- deploy.sh
+++ deploy.sh
@@ -1,19 +1,38 @@
 #!/bin/bash
 
 set -o errexit
-set -o nounset
 
 readonly AWS_PROFILE=$1
 readonly AWS_BUCKET="s3://documentation.hioperator.com"
 readonly BUILD_DIR="./build"
 
-if [ -d "${BUILD_DIR}" ]; then
-    rm -r "${BUILD_DIR}"
-fi
-# check if user has yarn
-# if not use npm to build the documentation
-if [ -z `which yarn` ]; then
-    npm run build
-fi
-yarn run build
-aws --profile "${AWS_PROFILE}" s3 sync "${BUILD_DIR}/." "${AWS_BUCKET}"
+usage() {
+cat << EOF
+usage for $0
+===================
+
+./deploy.sh "AWS PROFILE GOES HERE"
+EOF
+    exit 1
+}
+
+main() {
+    if [ "$1" -lt 1 ]; then
+        usage
+    fi
+    # check if build dir exists
+    if [ -d "${BUILD_DIR}" ]; then
+        # remove if it does
+        echo "removing old build dir..."
+        rm -r "${BUILD_DIR}"
+    fi
+    # check if user has yarn
+    if [ -z `which yarn` ]; then
+        # if not use npm to build the documentation
+        npm run build
+    fi
+    yarn run build
+    aws --profile "${AWS_PROFILE}" s3 sync "${BUILD_DIR}/." "${AWS_BUCKET}"
+    exit 0
+}
+main $#
